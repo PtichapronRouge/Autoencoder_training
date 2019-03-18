ac4a6433f1dbb5c46ae40c0e57506a83c5c1d3c7
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2019-03-18 18:57:38 +0100

    Test d'autoencodeur pour kingfisher, format=Top

---
+++ kingfisher_AE_top/autoencoder.py
@@ -0,0 +1,33 @@
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras import backend as K
+import numpy as np
+
+data_path = "/tmp/data.npy"
+if len(sys.argv > 1):
+    data_path = str(sys.argv[1])
+
+data = np.load(data_path)
+#shape = (2451, 512, 512)
+image_size = data.shape[1]
+input_shape=(image_size,image_size,1)
+
+X_train = np.reshape(X_train, [-1, image_size, image_size, 1])
+X_train = X_train.astype('float32')/255
+
+input_img = Input(shape=input_shape)
+
+x = Conv2D(16, (5,5), activation='relu', padding='same')(input_img) #512*512
+x = MaxPooling2D((4,4), padding='same')(x)                          #128*128
+x = Conv2D(8, (5,5), activation='relu', padding='same')(x)          #128*128
+encoded = MaxPooling2D((4,4), padding='same')(x)                    #32*32
+
+x = Conv2D(8, (5,5), activation='relu', padding='same')(encoded)
+x = UpSampling2D((4,4))(x)
+x = Conv2D(16, (5,5), activation='relu', padding='same')(x)
+x = UpSampling2D((4,4))(x)
+decoded = Conv2D(1, (5,5), activation='relu', padding='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
